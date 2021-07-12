02b44b88d20aa77cee2dc1e4f6692949d459c27e
Richar A. Macedo <richar.silvamacedo@gmail.com>
2021-07-12 19:58:51 -0300

    Last build + change image px to vw

---
+++ docs/contact.a6917d84.js
@@ -0,0 +1,2 @@
+!function(){function a(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],a("js",new Date),a("config","G-6P13KWD4N0")}();
+//# sourceMappingURL=contact.a6917d84.js.map
