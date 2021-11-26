b8b880c216cc7a6d566f32a63defb62a0e9e18cb
Richar A. Macedo <richar.silvamacedo@gmail.com>
2021-11-26 15:17:50 -0400

    #5 finished portfolio

--- docs/eventene.abf60e97.js
+++ docs/eventene.abf60e97.js
@@ -1,2 +1,2 @@
 !function(){for(var e,r=document.querySelectorAll(".carousel__container"),o=0;o<r.length;o++){var t=r[o];void 0,e=void 0,void 0,{prevNextButtons:!1,percentPosition:!1,setGallerySize:!0,wrapAround:!0,resize:!0},e=t.querySelector(".carousel"),new Flickity(e,{prevNextButtons:!1,percentPosition:!1,setGallerySize:!0,wrapAround:!0,resize:!0}).on("select")}}();
-//# sourceMappingURL=index.cdac6f66.js.map
+//# sourceMappingURL=eventene.abf60e97.js.map
